2809acc56c7da7a1b534057c53552f257923b6db
JackDCollins <collinj6@tcd.ie>
2018-11-24 21:26:46 +0000

    Completed Git Commit Page - using playgrpound.py

--- playground.py
+++ playground.py
@@ -1,7 +1,7 @@
 from tkinter import *
 from tkinter import ttk
 from github import Github
-
+import csv
 
 def getRepoDetails(user):
 
@@ -122,15 +122,15 @@ def getLangSkills(user) :
 
 
 class Commit(object):
-    name = ""
+    author = ""
     #insertions = 0
     #deletions = 0
     date = ""
 
 
     # The class "constructor" - It's actually an initializer
-    def __init__(self, name,date):
-        self.name = name
+    def __init__(self, author,date):
+        self.author = author
         #self.insertions = insertions
         #self.deletions = deletions
         self.date = date
@@ -141,7 +141,7 @@ g = Github("JackDCollins", "@ppl3Inc")
 repos =  g.get_user().get_repos()
 #print(getLangSkills(g))
 #for repo in repos:
-repo = g.get_repo("JackDCollins/CS3012")
+repo = g.get_repo("SeanFitz1997/EBII1819--Trinity_Module_Review")
 #print(dir(repos))
 c = repo.get_commits()
 
@@ -149,11 +149,25 @@ l = list()
 for ci in c:
         #print(dir(ci))
         #print(dir(ci.author))
-    date = ci.commit.author.date
-    author = ci.author.login
-    commit = Commit(author,date)
-    l.append(commit)
-    #print(dir(ci))
-    files = ci.files
-    for file in files  :
-        #print(file.filename)
+    if ci.author!= None:
+        date = ci.commit.author.date
+        author = ci.author.login
+        commit = Commit(author,date)
+        l.append(commit)
+        #print(dir(ci))
+
+dict = {}
+for c in l :
+        if dict.get(c.author) == None:
+            dict[c.author] = 1
+        else :
+            dict[c.author] = dict[c.author]+1
+
+print(dict)
+
+with open('commits.csv', 'w') as csvfile:
+    fieldnames = ['name', 'val']
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    writer.writeheader()
+    for key, val in dict.items() :
+        writer.writerow({'name': key, 'val': val})
