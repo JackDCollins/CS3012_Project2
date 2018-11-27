14638f7fc358939cbde64a4d51ee89d195846cfa
JackDCollins <collinj6@tcd.ie>
2018-11-27 21:18:33 +0000

    Added new Grapher.py to handle all d3.js code with new plugins

---
+++ Grapher.py
@@ -0,0 +1,25 @@
+import plotly.offline as offline
+import plotly.graph_objs as go
+import plotly.tools as tls
+import matplotlib.pyplot as plt
+import os
+
+#import matplotlib.pyplot as plt
+#inputDataionary = plt.figure()
+#inputData= { "A":1 , "B":2}
+#data = [go.Bar(x=[inputData.keys()],y=[20, 14, 23])]
+
+def doCommitGraph(inputData) :
+        #a = t + (v)
+    data = [go.Bar(
+            x=tuple(inputData.keys()),
+            y=tuple(inputData.values()),marker=dict(
+        color='#4F7CAC'
+        )
+    )]
+
+    layout = go.Layout(title='Number of Commits per Contributor')
+
+    fig = go.Figure(data=data, layout=layout)
+    offline.plot(fig)
+    os.rename('temp-plot.html', 'commitGraph.html')
--- MainCode.py
+++ MainCode.py
@@ -1,19 +1,29 @@
 from tkinter import *
 from tkinter import ttk
 from github import Github
+import Grapher as gra
 import csv
 
 class Commit(object):
     author = ""
-    #insertions = 0
-    #deletions = 0
     date = ""
-    # The class "constructor" - It's actually an initializer
+
     def __init__(self, author,date):
         self.author = author
-        #self.insertions = insertions
-        #self.deletions = deletions
         self.date = date
+
+class CommitChurn(object):
+    author = ""
+    insertions=0
+    deletions=0
+    date = ""
+
+    def __init__(self, author,date,insertions, deletions):
+        self.author = author
+        self.date = date
+        self.insertions = insertions
+        self.deletions = deletions
+
 
 
 def doCommitHistory(github,repos):
@@ -30,7 +40,9 @@ def doCommitHistory(github,repos):
             if dict.get(commit.author) == None:
                 dict[commit.author] = 1
             else :
-                dict[commit.author] = dict[c.author]+1
+                dict[commit.author] = dict[commit.author]+1
+
+    gra.doCommitGraph(dict)
 
     with open('commits.csv', 'w') as csvfile:
         fieldnames = ['name', 'val']
@@ -40,9 +52,38 @@ def doCommitHistory(github,repos):
             writer.writerow({'name': key, 'val': val})
 
 
+def doCodeChurn(github,repos):
+    l = list()
+    for commit in repos:
+        if commit.author!= None:
+            date = commit.commit.author.date
+            author = commit.author.login
+            ins = commit.stats.additions
+            dels = commit.stats.deletions
+            commitObject = CommitChurn(author,date,ins,dels)
+            l.append(commitObject)
+
+    tmp = 0
+    dict = {}
+    for commit in l :
+            change = commit.insertions - commit.deletions
+            dict[commit.date] = tmp + change
+            tmp = tmp + change
+
+
+    with open('codechurn.csv', 'w') as csvfile:
+        fieldnames = ['date', 'close']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        for key, val in dict.items() :
+            writer.writerow({'date': key.strftime("%m/%d/%y"), 'close': val})
+
+
 
 g = Github("b94cfbadca124937e3a619bcc3a90b3c4fb119e0")
-repos =  g.get_user().get_repos()
+#repos =  g.get_user().get_repos()
 repo = g.get_repo("SeanFitz1997/EBII1819--Trinity_Module_Review")
 c = repo.get_commits()
+#doCommitHistory(g,c)
+#doCodeChurn(g,c)
 doCommitHistory(g,c)
